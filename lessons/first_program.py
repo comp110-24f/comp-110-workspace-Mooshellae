f27287093fa8f8959be7b348f9f3523706512254
Michelle Hu <michu@unc.edu>
2024-08-21 15:37:42 -0400

    Finished Exercise 00!

---
+++ lessons/first_program.py
@@ -0,0 +1,9 @@
+"""Greeting"""
+
+print("Hello World")
+# terminal below - interact with computer through text commands (e.g. find things)
+# terminal: clear - deletes everything
+# must name module w/ .py to make sure it's a python file
+# ways to play program: 1) play, 2) terminal: python -m lessons.first_program, 3) trailhead > opens in new browser
+# ctrl c to close trailhead server
+# REPL (read execute print loop): type python in term to executy python commands, quit() - unsaved: good for quick comps/testing new concepts
